4e120f727ea2b92ab76ea2625cf597a2c8629233
Roman <pandarplayso2@gmail.com>
2025-02-14 12:27:30 +0200

    2 завдання

--- 12345-/12345-.py
+++ 12345-/12345-.py
@@ -1,1 +1,7 @@
-print("Привіт, викладачу!")
+try:
+    user_input = input("Введіть число: ")
+    number = int(user_input)
+    print(f"Ви ввели число: {number}")
+except ValueError:
+    print("Помилка! Введено нечислові дані.")
+
